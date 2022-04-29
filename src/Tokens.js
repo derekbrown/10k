340c431ba56269e4ab6e7b24b042f7e339e88b29
Derek Brown <derek@brown.lt>
2022-04-29 18:19:41 -0500

    Grid

--- src/Tokens.js
+++ src/Tokens.js
@@ -4,7 +4,7 @@ import { supportedTokens } from './supportedTokenList';
 
 function Tokens({number}) {
   return (
-    <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 h-full w-full font-semibold">
+    <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 h-full w-full font-semibold">
       {supportedTokens && supportedTokens.length > 0 && supportedTokens.map(item =>
         <NFT
           contractAddress={item.contractAddress}
