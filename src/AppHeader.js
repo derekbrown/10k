a05641d2c2f7a439a2b7e383a27015278f41ed6b
Derek Brown <derek@brown.lt>
2022-04-29 17:57:46 -0500

    Fix NumberInput

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -7,7 +7,7 @@ function AppHeader({handleChange, number}) {
     <nav className="flex flex-row mb-8 justify-between w-full items-center">
       <div className="flex flex-row items-center">
         <h2 className="bebas text-6xl text-white font-bold">10K</h2>
-        <input className="md:w-72 lg:w-96 ml-8 p-2 rounded -mt-1" placeholder="Lookup Number" type="text" />
+        <input className="md:w-72 lg:w-96 ml-8 p-2 rounded -mt-1" placeholder="Lookup Number" type="text" onChange={handleChange}/>
       </div>
       <div className="flex flex-row items-center">
         <a href="https://twitter.com/10kClubOfficial" target="_blank" rel="noreferrer"><img src={TWITTER} className="h-8 w-8 mx-4" alt="Visit 10KClub on Twitter"/></a>
